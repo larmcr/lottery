52f207b6cad0ac9bd603477190ca708490d5e47b
larmcr <larmcr@gmail.com>
2023-02-25 23:14:18 -0600

    Update lotto.js

--- lotto.js
+++ lotto.js
@@ -5,7 +5,7 @@ const getData = async () => {
   const API = 'https://integration.jps.go.cr/api/app/lotto';
   let flag = true;
   const getJson = async (id) => {
-    console.info(`Fetching '${id}'`);
+    console.info(`> Fetching '${id}'`);
     try {
       const response = await fetch(`${API}/${id}`);
       const json = await response.json();
@@ -13,19 +13,31 @@ const getData = async () => {
     }
     catch(err) {
       flag = false;
-      console.info('ERROR ->', err);
+      console.info(`> ERROR -> ${err}`);
+    }
+  }
+  const saveJson = (json) => {
+    const num = json.numeroSorteo;
+    const file = `./data/${num}.json`;
+    if (!fs.existsSync(file)) {
+      fs.writeFileSync(file, JSON.stringify(json));
+      console.info(`  > '${num}' saved`);
+    } else {
+      flag = false;
+      console.info(`  > '${num}' not saved (already exists)`);
     }
   }
   const last = await getJson('last');
-  fs.writeFileSync(`./data/${last.numeroSorteo}.json`, JSON.stringify(last));
+  saveJson(last);
   const max = last.numeroSorteo - 1;
   for (let sorteo = max; flag; --sorteo) {
     const json = await getJson(sorteo);
-    fs.writeFileSync(`./data/${json.numeroSorteo}.json`, JSON.stringify(json));
+    saveJson(json);
   }
 }
 
 const processData = () => {
+  console.info(`> Processing data`);
   const files = fs.readdirSync('./data');
   let lottos = {};
   files.forEach((file) => {
@@ -33,9 +45,10 @@ const processData = () => {
     lottos[numeroSorteo] = { n: numeros, r: numerosRevancha };
   });
   fs.writeFileSync('./lotto.json', JSON.stringify(lottos));
+  console.info(`  > Done!`);
 }
 
 (async () => {
-  // await getData();
+  await getData();
   processData();
 })();
