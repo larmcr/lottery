9718654907501a4622f770649537e02fe1b0bfb1
larmcr <larmcr@gmail.com>
2025-03-24 09:13:33 -0600

    Data

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,3 +1,5 @@
 SELECT orden, numero, COUNT(numero) as cantidad FROM monazos GROUP BY orden, numero
 
 SELECT sorteo, orden, numero FROM monazos ORDER BY sorteo, orden
+
+SELECT numero, COUNT() as cantidad FROM lottos GROUP BY numero ORDER BY cantidad DESC
